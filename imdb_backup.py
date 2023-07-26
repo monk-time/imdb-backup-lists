9fa7fbb03b93494770097b2ae1ac71176f6114fd
Dmitry <monk-time@users.noreply.github.com>
2023-07-26 23:17:17 +0300

    Fix formatting with black

--- imdb_backup.py
+++ imdb_backup.py
@@ -15,8 +15,10 @@ from bs4 import BeautifulSoup
 REQUIRED_COOKIES = {'at-main', 'ubid-main', 'uu'}
 COOKIE_FNAME = 'imdb_cookie.json'
 ZIP_FNAME = 'imdb_exported_lists.zip'
-README_REF = 'For more info check README.md.\n' \
-             '[https://github.com/monk-time/imdb-backup-lists/blob/master/README.md]'
+README_REF = (
+    'For more info check README.md.\n'
+    '[https://github.com/monk-time/imdb-backup-lists/blob/master/README.md]'
+)
 
 MList = Dict[str, Union[str, bytes]]
 
@@ -24,7 +26,8 @@ MList = Dict[str, Union[str, bytes]]
 def slugify(s: str) -> str:
     """
     Convert to lowercase ASCII with hyphens instead of underscores or spaces.
-    Remove all non-alphanumeric characters and strip leading and trailing whitespace.
+    Remove all non-alphanumeric characters and strip
+    leading and trailing whitespace.
     """
     s = unidecode.unidecode(s)
     s = re.sub(r'[^\w\s-]', '', s).strip().lower()
@@ -33,28 +36,37 @@ def slugify(s: str) -> str:
 
 
 def load_imdb_cookies(cookie_path):
-    """Read an IMDb 'id' cookie from the folder containing the script or executable."""
+    """Read an IMDb 'id' cookie from the folder
+    containing the script or executable.
+    """
     # https://pyinstaller.readthedocs.io/en/stable/runtime-information.html#using-sys-executable-and-sys-argv-0
     if cookie_path.exists():
         cookies = json.loads(cookie_path.read_text())
         if not REQUIRED_COOKIES <= set(cookies):
-            raise ValueError(f'\n\n{COOKIE_FNAME} must contain the following cookies: '
-                             f'{", ".join(REQUIRED_COOKIES)}.')
+            raise ValueError(
+                f'\n\n{COOKIE_FNAME} must contain the following cookies: '
+                f'{", ".join(REQUIRED_COOKIES)}.'
+            )
         return cookies
-    else:
-        raise FileNotFoundError(f'\n\nCreate a file "{COOKIE_FNAME}" in the script directory\n'
-                                f'and put your IMDb cookie inside.\n{README_REF}')
+    raise FileNotFoundError(
+        f'\n\nCreate a file "{COOKIE_FNAME}" in the script directory\n'
+        f'and put your IMDb cookie inside.\n{README_REF}'
+    )
 
 
 def fetch_userid(cookies: dict) -> str:
-    """User ID is required for exporting any lists. Cookie validity will also be checked here."""
+    """User ID is required for exporting any lists.
+    Cookie validity will also be checked here.
+    """
     r = requests.head('https://www.imdb.com/profile', cookies=cookies)
     r.raise_for_status()
     m = re.search(r'ur\d+', r.headers['Location'])
     if not m:
-        raise Exception("\n\nCan't log into IMDb.\n"
-                        f'Make sure that your IMDb cookie in {COOKIE_FNAME} is correct.\n'
-                        f'{README_REF}')
+        raise Exception(
+            "\n\nCan't log into IMDb.\n"
+            f'Make sure that your IMDb cookie in {COOKIE_FNAME} is correct.\n'
+            f'{README_REF}'
+        )
     return m.group()
 
 
@@ -62,41 +74,58 @@ def get_fname(url: str, title: str) -> str:
     """Turn an IMDb list into {LIST_OR_USER_ID}_{TITLE_SLUG}.csv."""
     match = re.search(r"..\d{6,}", url, re.MULTILINE)
     if not match:
-        raise Exception(f'\n\nCan\'t extract list/user ID from {url} for the list "{title}"')
+        raise Exception(
+            f'\n\nCan\'t extract list/user ID from {url} '
+            f'for the list "{title}"'
+        )
     return match.group() + '_' + slugify(title) + '.csv'
 
 
-def fetch_lists_info(userid: str, cookies: dict) -> Generator[Dict, None, None]:
-    r = requests.get(f'https://www.imdb.com/user/{userid}/lists', cookies=cookies)
+def fetch_lists_info(
+    userid: str, cookies: dict
+) -> Generator[Dict, None, None]:
+    r = requests.get(
+        f'https://www.imdb.com/user/{userid}/lists', cookies=cookies
+    )
     r.raise_for_status()
 
     # Fetch two special lists: ratings and watchlist
     # /lists has an old link for ratings; easier to hardcode it
-    yield {'url': f'/user/{userid}/ratings/',
-           'fname': get_fname(userid, 'ratings'),
-           'title': 'Ratings'}
-    # /lists doesn't have a link for watchlist that can be used for exporting at all
-    r_wl = requests.get(f'https://www.imdb.com/user/{userid}/watchlist', cookies=cookies)
-    listid = BeautifulSoup(r_wl.text, 'html.parser').find('meta', property='pageId').get('content')
-    yield {'url': f'/list/{listid}/',
-           'fname': get_fname(userid, 'watchlist'),
-           'title': 'Watchlist'}
+    yield {
+        'url': f'/user/{userid}/ratings/',
+        'fname': get_fname(userid, 'ratings'),
+        'title': 'Ratings',
+    }
+    # /lists doesn't have a link for watchlist that can be used for exporting
+    r_wl = requests.get(
+        f'https://www.imdb.com/user/{userid}/watchlist', cookies=cookies
+    )
+    listid = (
+        BeautifulSoup(r_wl.text, 'html.parser')
+        .find('meta', property='pageId')
+        .get('content')
+    )
+    yield {
+        'url': f'/list/{listid}/',
+        'fname': get_fname(userid, 'watchlist'),
+        'title': 'Watchlist',
+    }
 
     # Fetch the rest of user's lists
     links = BeautifulSoup(r.text, 'html.parser').select('a.list-name')
     for link in links:
         url = link.get('href')
         title = link.string
-        yield {'url': url,
-               'fname': get_fname(url, title),
-               'title': title}
+        yield {'url': url, 'fname': get_fname(url, title), 'title': title}
 
 
 def export(mlist: MList, cookies: dict) -> MList:
     """All requests are throttled just in case."""
     time.sleep(0.5)
     print('Downloading:', mlist['title'].replace('\n', ' '))
-    r = requests.get(f'https://www.imdb.com{mlist["url"]}export', cookies=cookies)
+    r = requests.get(
+        f'https://www.imdb.com{mlist["url"]}export', cookies=cookies
+    )
     r.raise_for_status()
     mlist['content'] = r.content
     return mlist
@@ -105,14 +134,16 @@ def export(mlist: MList, cookies: dict) -> MList:
 def zip_all(mlists: Iterable[MList], zip_fname=ZIP_FNAME):
     """Write all downloaded movielists into a zip archive.
 
-    A file with original list names (quoted if multi-line) is also added to the archive.
+    A file with original list names (quoted if multi-line) is also added.
     """
-    with zipfile.ZipFile(zip_fname, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+    with zipfile.ZipFile(
+        zip_fname, mode='w', compression=zipfile.ZIP_DEFLATED
+    ) as zf:
         titles = []
         for ml in mlists:
             print('  ->', ml['fname'])
             zf.writestr(ml['fname'], ml['content'])
-            # After the Dec'17 redesign lists on IMDb can have multi-line titles
+            # After the Dec'17 redesign IMDb lists can have multi-line titles
             title = ml['title']
             if '\n' in title:
                 # zipfile.writestr doesn't do automatic line ending conversion
@@ -141,19 +172,29 @@ def pause_before_exit_unless_run_with_flag():
         input('\nPress <ENTER> to exit ... ')
 
     import argparse
+
     parser = argparse.ArgumentParser()
     # Optional positional argument for the input file with cookies
 
     # noinspection PyTypeChecker
-    parser.add_argument('path', nargs='?', type=Path,
-                        default=Path(sys.argv[0]).resolve().parent / COOKIE_FNAME,
-                        help="path to the .json file with IMDb cookies")
-    parser.add_argument('-n', '--nopause', action='store_true',
-                        help="don't pause the script before exiting")
+    parser.add_argument(
+        'path',
+        nargs='?',
+        type=Path,
+        default=Path(sys.argv[0]).resolve().parent / COOKIE_FNAME,
+        help="path to the .json file with IMDb cookies",
+    )
+    parser.add_argument(
+        '-n',
+        '--nopause',
+        action='store_true',
+        help="don't pause the script before exiting",
+    )
 
     args = parser.parse_args()
     if not args.nopause:
         import atexit
+
         atexit.register(prompt)
 
     backup(cookie_path=args.path)
